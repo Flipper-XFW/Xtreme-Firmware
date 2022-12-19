128cca0613668740250ce0c9d513d5f4eb4dc270
VerstreuteSeele <fischi.gk@gmail.com>
2022-12-19 20:09:57 +0100

    Clock locale

--- fbt_options.py
+++ fbt_options.py
@@ -14,7 +14,7 @@ DEBUG = 0
 
 # Suffix to add to files when building distribution
 # If OS environment has DIST_SUFFIX set, it will be used instead
-DIST_SUFFIX = "CC_CL-0017_12192022"
+DIST_SUFFIX = "CC_CL-0018_12192022"
 
 # Coprocessor firmware
 COPRO_OB_DATA = "scripts/ob.data"
