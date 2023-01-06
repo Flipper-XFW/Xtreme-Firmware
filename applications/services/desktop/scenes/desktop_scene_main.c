bf6ed203dbe04f3b41d1fa16fc9188910420d7c3
jbohack <joebohack@gmail.com>
2023-01-05 21:46:09 -0500

    fixed clock app & added subghz playlist icon back

--- applications/services/desktop/scenes/desktop_scene_main.c
+++ applications/services/desktop/scenes/desktop_scene_main.c
@@ -220,7 +220,7 @@ bool desktop_scene_main_on_event(void* context, SceneManagerEvent event) {
             break;
         }
         case DesktopMainEventOpenClock: {
-            desktop_scene_main_open_app_or_profile(desktop, EXT_PATH("/apps/Main/Clock.fap"));
+            desktop_scene_main_open_app_or_profile(desktop, EXT_PATH("/apps/Tools/Clock.fap"));
             break;
         }
         case DesktopMainEventOpenTetris: {
