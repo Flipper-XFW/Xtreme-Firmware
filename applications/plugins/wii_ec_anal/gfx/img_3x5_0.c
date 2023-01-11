de8bdd4469bcb11401af1c863a66b68989dfd8b1
VerstreuteSeele <fischi.gk@gmail.com>
2023-01-11 01:51:39 +0100

    update Wii EC

--- applications/plugins/wii_ec_anal/gfx/img_3x5_0.c
+++ applications/plugins/wii_ec_anal/gfx/img_3x5_0.c
@@ -6,4 +6,6 @@
 
 #include "images.h"
 
-const image_t img_3x5_0 = {3, 5, false, 2, 0, {0xF6, 0xDE}};
+const image_t  img_3x5_0 = { 3, 5, false, 2, 0, {
+	0xF6, 0xDE
+}};
--- applications/plugins/wii_ec_anal/gfx/img_3x5_3.c
+++ applications/plugins/wii_ec_anal/gfx/img_3x5_3.c
@@ -6,4 +6,6 @@
 
 #include "images.h"
 
-const image_t img_3x5_3 = {3, 5, false, 2, 0, {0xE5, 0x9E}};
+const image_t  img_3x5_3 = { 3, 5, false, 2, 0, {
+	0xE5, 0x9E
+}};
