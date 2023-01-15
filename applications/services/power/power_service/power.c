5802ea951def42b193438e45cb373684862c905f
Willy-JL <willy.leslie@icloud.com>
2023-01-15 23:57:18 +0000

    Add tiny battery border for readability

--- applications/services/power/power_service/power.c
+++ applications/services/power/power_service/power.c
@@ -9,7 +9,14 @@
 void power_draw_battery_callback(Canvas* canvas, void* context) {
     furi_assert(context);
     Power* power = context;
-    canvas_draw_icon(canvas, 0, 0, &I_Battery_26x8);
+    canvas_draw_icon(canvas, 0, 0, &I_Battery_25x8);
+    canvas_set_color(canvas, ColorWhite);
+    canvas_draw_box(canvas, -1, 0, 1, 8);
+    canvas_draw_box(canvas, 0, -1, 24, 1);
+    canvas_draw_box(canvas, 0, 8, 24, 1);
+    canvas_draw_box(canvas, 25, 1, 2, 6);
+    canvas_set_color(canvas, ColorBlack);
+    canvas_draw_box(canvas, 25, 2, 1, 4);
 
     if(power->info.gauge_is_ok) {
         char batteryPercentile[4];
@@ -168,7 +175,7 @@ void power_draw_battery_callback(Canvas* canvas, void* context) {
 
 static ViewPort* power_battery_view_port_alloc(Power* power) {
     ViewPort* battery_view_port = view_port_alloc();
-    view_port_set_width(battery_view_port, icon_get_width(&I_Battery_26x8));
+    view_port_set_width(battery_view_port, icon_get_width(&I_Battery_25x8));
     view_port_draw_callback_set(battery_view_port, power_draw_battery_callback, power);
     gui_add_view_port(power->gui, battery_view_port, GuiLayerStatusBarRight);
     return battery_view_port;
