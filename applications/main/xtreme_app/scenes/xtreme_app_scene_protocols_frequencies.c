be63934eae01fb11c028df56e1d059d618c4884d
Willy-JL <willy.leslie@icloud.com>
2023-03-06 14:36:52 +0000

    Move static and hopper frequencies to submenus

--- applications/main/xtreme_app/scenes/xtreme_app_scene_protocols_frequencies.c
+++ applications/main/xtreme_app/scenes/xtreme_app_scene_protocols_frequencies.c
@@ -2,12 +2,8 @@
 
 enum VarItemListIndex {
     VarItemListIndexUseDefaults,
-    VarItemListIndexStaticFrequency,
-    VarItemListIndexDeleteStaticFreq,
-    VarItemListIndexAddStaticFreq,
-    VarItemListIndexHopperFrequency,
-    VarItemListIndexDeleteHopperFreq,
-    VarItemListIndexAddHopperFreq,
+    VarItemListIndexStaticFrequencies,
+    VarItemListIndexHopperFrequencies,
 };
 
 void xtreme_app_scene_protocols_frequencies_var_item_list_callback(void* context, uint32_t index) {
@@ -23,24 +19,6 @@ static void xtreme_app_scene_protocols_frequencies_use_defaults_changed(Variable
     app->save_subghz_frequencies = true;
 }
 
-static void xtreme_app_scene_protocols_frequencies_static_frequency_changed(VariableItem* item) {
-    XtremeApp* app = variable_item_get_context(item);
-    app->subghz_static_index = variable_item_get_current_value_index(item);
-    uint32_t value = *FrequencyList_get(app->subghz_static_frequencies, app->subghz_static_index);
-    char text[10] = {0};
-    snprintf(text, sizeof(text), "%lu.%02lu", value / 1000000, (value % 1000000) / 10000);
-    variable_item_set_current_value_text(item, text);
-}
-
-static void xtreme_app_scene_protocols_frequencies_hopper_frequency_changed(VariableItem* item) {
-    XtremeApp* app = variable_item_get_context(item);
-    app->subghz_hopper_index = variable_item_get_current_value_index(item);
-    uint32_t value = *FrequencyList_get(app->subghz_hopper_frequencies, app->subghz_hopper_index);
-    char text[10] = {0};
-    snprintf(text, sizeof(text), "%lu.%02lu", value / 1000000, (value % 1000000) / 10000);
-    variable_item_set_current_value_text(item, text);
-}
-
 void xtreme_app_scene_protocols_frequencies_on_enter(void* context) {
     XtremeApp* app = context;
     VariableItemList* var_item_list = app->var_item_list;
@@ -51,37 +29,9 @@ void xtreme_app_scene_protocols_frequencies_on_enter(void* context) {
     variable_item_set_current_value_index(item, app->subghz_use_defaults);
     variable_item_set_current_value_text(item, app->subghz_use_defaults ? "ON" : "OFF");
 
-    item = variable_item_list_add(var_item_list, "Static Freq", FrequencyList_size(app->subghz_static_frequencies), xtreme_app_scene_protocols_frequencies_static_frequency_changed, app);
-    app->subghz_static_index = 0;
-    variable_item_set_current_value_index(item, app->subghz_static_index);
-    if(FrequencyList_size(app->subghz_static_frequencies)) {
-        uint32_t value = *FrequencyList_get(app->subghz_static_frequencies, app->subghz_static_index);
-        char text[10] = {0};
-        snprintf(text, sizeof(text), "%lu.%02lu", value / 1000000, (value % 1000000) / 10000);
-        variable_item_set_current_value_text(item, text);
-    } else {
-        variable_item_set_current_value_text(item, "None");
-    }
+    variable_item_list_add(var_item_list, "Static Frequencies", 0, NULL, app);
 
-    variable_item_list_add(var_item_list, "Delete Static Freq", 0, NULL, app);
-
-    variable_item_list_add(var_item_list, "Add Static Freq", 0, NULL, app);
-
-    item = variable_item_list_add(var_item_list, "Hopper Freq", FrequencyList_size(app->subghz_hopper_frequencies), xtreme_app_scene_protocols_frequencies_hopper_frequency_changed, app);
-    app->subghz_hopper_index = 0;
-    variable_item_set_current_value_index(item, app->subghz_hopper_index);
-    if(FrequencyList_size(app->subghz_hopper_frequencies)) {
-        uint32_t value = *FrequencyList_get(app->subghz_hopper_frequencies, app->subghz_hopper_index);
-        char text[10] = {0};
-        snprintf(text, sizeof(text), "%lu.%02lu", value / 1000000, (value % 1000000) / 10000);
-        variable_item_set_current_value_text(item, text);
-    } else {
-        variable_item_set_current_value_text(item, "None");
-    }
-
-    variable_item_list_add(var_item_list, "Delete Hopper Freq", 0, NULL, app);
-
-    variable_item_list_add(var_item_list, "Add Hopper Freq", 0, NULL, app);
+    variable_item_list_add(var_item_list, "Hopper Frequencies", 0, NULL, app);
 
     variable_item_list_set_enter_callback(
         var_item_list, xtreme_app_scene_protocols_frequencies_var_item_list_callback, app);
@@ -92,23 +42,6 @@ void xtreme_app_scene_protocols_frequencies_on_enter(void* context) {
     view_dispatcher_switch_to_view(app->view_dispatcher, XtremeAppViewVarItemList);
 }
 
-void remove_frequency(XtremeApp* app, FrequencyList_t list, uint8_t index) {
-    if(!FrequencyList_size(list)) return;
-    uint32_t value = *FrequencyList_get(list, index);
-    FrequencyList_it_t it;
-    FrequencyList_it(it, list);
-    while(!FrequencyList_end_p(it)) {
-        if(*FrequencyList_ref(it) == value) {
-            FrequencyList_remove(list, it);
-        } else {
-            FrequencyList_next(it);
-        }
-    }
-    app->save_subghz_frequencies = true;
-    scene_manager_previous_scene(app->scene_manager);
-    scene_manager_next_scene(app->scene_manager, XtremeAppSceneProtocolsFrequencies);
-}
-
 bool xtreme_app_scene_protocols_frequencies_on_event(void* context, SceneManagerEvent event) {
     XtremeApp* app = context;
     bool consumed = false;
@@ -117,19 +50,11 @@ bool xtreme_app_scene_protocols_frequencies_on_event(void* context, SceneManager
         scene_manager_set_scene_state(app->scene_manager, XtremeAppSceneProtocolsFrequencies, event.event);
         consumed = true;
         switch(event.event) {
-        case VarItemListIndexDeleteStaticFreq:
-            remove_frequency(app, app->subghz_static_frequencies, app->subghz_static_index);
+        case VarItemListIndexStaticFrequencies:
+            scene_manager_next_scene(app->scene_manager, XtremeAppSceneProtocolsFrequenciesStatic);
             break;
-        case VarItemListIndexAddStaticFreq:
-            scene_manager_set_scene_state(app->scene_manager, XtremeAppSceneProtocolsFrequenciesAdd, false);
-            scene_manager_next_scene(app->scene_manager, XtremeAppSceneProtocolsFrequenciesAdd);
-            break;
-        case VarItemListIndexDeleteHopperFreq:
-            remove_frequency(app, app->subghz_hopper_frequencies, app->subghz_hopper_index);
-            break;
-        case VarItemListIndexAddHopperFreq:
-            scene_manager_set_scene_state(app->scene_manager, XtremeAppSceneProtocolsFrequenciesAdd, true);
-            scene_manager_next_scene(app->scene_manager, XtremeAppSceneProtocolsFrequenciesAdd);
+        case VarItemListIndexHopperFrequencies:
+            scene_manager_next_scene(app->scene_manager, XtremeAppSceneProtocolsFrequenciesHopper);
             break;
         default:
             break;
