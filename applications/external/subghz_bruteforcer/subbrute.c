4229aab7e9da94a68c4662dbdbe37468f47df218
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-04-25 17:50:41 +0100

    Update subbrute

--- applications/external/subghz_bruteforcer/subbrute.c
+++ applications/external/subghz_bruteforcer/subbrute.c
@@ -185,7 +185,8 @@ int32_t subbrute_app(void* p) {
     // Auto switch to internal radio if external radio is not available
     furi_delay_ms(15);
     if(!furi_hal_subghz_check_radio()) {
-        furi_hal_subghz_set_radio_type(SubGhzRadioInternal);
+        furi_hal_subghz_select_radio_type(SubGhzRadioInternal);
+        furi_hal_subghz_init_radio_type(SubGhzRadioInternal);
     }
 
     furi_hal_power_suppress_charge_enter();
@@ -195,6 +196,8 @@ int32_t subbrute_app(void* p) {
     furi_hal_power_suppress_charge_exit();
     // Disable power for External CC1101 if it was enabled and module is connected
     furi_hal_subghz_disable_ext_power();
+    // Reinit SPI handles for internal radio / nfc
+    furi_hal_subghz_init_radio_type(SubGhzRadioInternal);
 
     subbrute_free(instance);
 
