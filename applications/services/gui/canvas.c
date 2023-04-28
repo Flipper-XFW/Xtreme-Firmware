72a4fb6cb6ab7195af962cb8e638cfaeff73357f
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-04-28 16:53:49 +0100

    Begone, goofy ahh else-if chain

--- applications/services/gui/canvas.c
+++ applications/services/gui/canvas.c
@@ -140,18 +140,25 @@ void canvas_invert_color(Canvas* canvas) {
 void canvas_set_font(Canvas* canvas, Font font) {
     furi_assert(canvas);
     u8g2_SetFontMode(&canvas->fb, 1);
-    if(font == FontPrimary) {
-        u8g2_SetFont(&canvas->fb, u8g2_font_helvB08_tr);
-    } else if(font == FontSecondary) {
-        u8g2_SetFont(&canvas->fb, u8g2_font_haxrcorp4089_tr);
-    } else if(font == FontKeyboard) {
-        u8g2_SetFont(&canvas->fb, u8g2_font_profont11_mr);
-    } else if(font == FontBigNumbers) {
-        u8g2_SetFont(&canvas->fb, u8g2_font_profont22_tn);
-    } else if(font == FontBatteryPercent) {
-        u8g2_SetFont(&canvas->fb, u8g2_font_5x7_tf); //u8g2_font_micro_tr);
-    } else {
-        furi_crash(NULL);
+    switch(font) {
+        case FontPrimary:
+            u8g2_SetFont(&canvas->fb, u8g2_font_helvB08_tr);
+            break;
+        case FontSecondary:
+            u8g2_SetFont(&canvas->fb, u8g2_font_haxrcorp4089_tr);
+            break;
+        case FontKeyboard:
+            u8g2_SetFont(&canvas->fb, u8g2_font_profont11_mr);
+            break;
+        case FontBigNumbers:
+            u8g2_SetFont(&canvas->fb, u8g2_font_profont22_tn);
+            break;
+        case FontBatteryPercent:
+            u8g2_SetFont(&canvas->fb, u8g2_font_5x7_tf); //u8g2_font_micro_tr);
+            break;
+        default:
+            furi_crash(NULL);
+            break;
     }
 }
 
