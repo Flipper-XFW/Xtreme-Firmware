1e5c351dd6214b443d2c4cb6a1daf229996ed045
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-04-30 19:08:31 +0100

    Only load notification settings on normal boot

--- applications/services/notification/notification_app.c
+++ applications/services/notification/notification_app.c
@@ -538,8 +538,10 @@ int32_t notification_srv(void* p) {
     UNUSED(p);
     NotificationApp* app = notification_app_alloc();
 
-    if(!notification_load_settings(app)) {
-        notification_save_settings(app);
+    if(furi_hal_is_normal_boot()) {
+        if(!notification_load_settings(app)) {
+            notification_save_settings(app);
+        }
     }
 
     notification_vibro_off();
