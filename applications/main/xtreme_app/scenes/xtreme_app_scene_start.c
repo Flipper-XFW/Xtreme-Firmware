5c0b07c7b452fb49d2e8a94710d04307ac3aedb4
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-06-04 23:34:50 +0100

    Retry when failing to re-invoke first slideshow

--- applications/main/xtreme_app/scenes/xtreme_app_scene_start.c
+++ applications/main/xtreme_app/scenes/xtreme_app_scene_start.c
@@ -48,12 +48,15 @@ bool xtreme_app_scene_start_on_event(void* context, SceneManagerEvent event) {
             scene_manager_next_scene(app->scene_manager, XtremeAppSceneMisc);
             break;
         case VarItemListIndexVersion: {
-            if(storage_common_copy(
-                   furi_record_open(RECORD_STORAGE),
-                   EXT_PATH("dolphin/xfwfirstboot.bin"),
-                   EXT_PATH(".slideshow"))) {
-                app->show_slideshow = true;
-                xtreme_app_apply(app);
+            for(int i = 0; i < 10; i++) {
+                if(storage_common_copy(
+                       furi_record_open(RECORD_STORAGE),
+                       EXT_PATH("dolphin/xfwfirstboot.bin"),
+                       EXT_PATH(".slideshow"))) {
+                    app->show_slideshow = true;
+                    xtreme_app_apply(app);
+                    break;
+                }
             }
             break;
         }
