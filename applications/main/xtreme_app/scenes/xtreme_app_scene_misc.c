022e686e4a804e8a1a5f73b18b24b2304010462a
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-06-05 00:06:24 +0100

    Move dolphin options to own submenu

--- applications/main/xtreme_app/scenes/xtreme_app_scene_misc.c
+++ applications/main/xtreme_app/scenes/xtreme_app_scene_misc.c
@@ -2,47 +2,14 @@
 
 enum VarItemListIndex {
     VarItemListIndexScreen,
+    VarItemListIndexDolphin,
     VarItemListIndexChangeDeviceName,
-    VarItemListIndexDolphinLevel,
-    VarItemListIndexDolphinAngry,
-    VarItemListIndexButthurtTimer,
     VarItemListIndexChargeCap,
 };
 
 void xtreme_app_scene_misc_var_item_list_callback(void* context, uint32_t index) {
     XtremeApp* app = context;
     view_dispatcher_send_custom_event(app->view_dispatcher, index);
-}
-
-static void xtreme_app_scene_misc_dolphin_level_changed(VariableItem* item) {
-    XtremeApp* app = variable_item_get_context(item);
-    app->dolphin_level = variable_item_get_current_value_index(item) + 1;
-    char level_str[4];
-    snprintf(level_str, 4, "%li", app->dolphin_level);
-    variable_item_set_current_value_text(item, level_str);
-    app->save_level = true;
-}
-
-static void xtreme_app_scene_misc_dolphin_angry_changed(VariableItem* item) {
-    XtremeApp* app = variable_item_get_context(item);
-    app->dolphin_angry = variable_item_get_current_value_index(item);
-    char angry_str[4];
-    snprintf(angry_str, 4, "%li", app->dolphin_angry);
-    variable_item_set_current_value_text(item, angry_str);
-    app->save_angry = true;
-}
-
-const char* const butthurt_timer_names[] =
-    {"OFF", "30 M", "1 H", "2 H", "4 H", "6 H", "8 H", "12 H", "24 H", "48 H"};
-const uint32_t butthurt_timer_values[COUNT_OF(butthurt_timer_names)] =
-    {0, 1800, 3600, 7200, 14400, 21600, 28800, 43200, 86400, 172800};
-static void xtreme_app_scene_misc_butthurt_timer_changed(VariableItem* item) {
-    XtremeApp* app = variable_item_get_context(item);
-    uint8_t index = variable_item_get_current_value_index(item);
-    variable_item_set_current_value_text(item, butthurt_timer_names[index]);
-    XTREME_SETTINGS()->butthurt_timer = butthurt_timer_values[index];
-    app->save_settings = true;
-    app->require_reboot = true;
 }
 
 #define CHARGE_CAP_INTV 5
@@ -66,40 +33,10 @@ void xtreme_app_scene_misc_on_enter(void* context) {
     item = variable_item_list_add(var_item_list, "Screen", 0, NULL, app);
     variable_item_set_current_value_text(item, ">");
 
+    item = variable_item_list_add(var_item_list, "Dolphin", 0, NULL, app);
+    variable_item_set_current_value_text(item, ">");
+
     variable_item_list_add(var_item_list, "Change Device Name", 0, NULL, app);
-
-    char level_str[4];
-    snprintf(level_str, 4, "%li", app->dolphin_level);
-    item = variable_item_list_add(
-        var_item_list,
-        "Dolphin Level",
-        DOLPHIN_LEVEL_COUNT + 1,
-        xtreme_app_scene_misc_dolphin_level_changed,
-        app);
-    variable_item_set_current_value_index(item, app->dolphin_level - 1);
-    variable_item_set_current_value_text(item, level_str);
-
-    char angry_str[4];
-    snprintf(angry_str, 4, "%li", app->dolphin_angry);
-    item = variable_item_list_add(
-        var_item_list,
-        "Dolphin Angry",
-        BUTTHURT_MAX + 1,
-        xtreme_app_scene_misc_dolphin_angry_changed,
-        app);
-    variable_item_set_current_value_index(item, app->dolphin_angry);
-    variable_item_set_current_value_text(item, angry_str);
-
-    item = variable_item_list_add(
-        var_item_list,
-        "Butthurt Timer",
-        COUNT_OF(butthurt_timer_names),
-        xtreme_app_scene_misc_butthurt_timer_changed,
-        app);
-    value_index = value_index_uint32(
-        xtreme_settings->butthurt_timer, butthurt_timer_values, COUNT_OF(butthurt_timer_values));
-    variable_item_set_current_value_index(item, value_index);
-    variable_item_set_current_value_text(item, butthurt_timer_names[value_index]);
 
     char cap_str[6];
     value_index = xtreme_settings->charge_cap / CHARGE_CAP_INTV;
@@ -132,6 +69,9 @@ bool xtreme_app_scene_misc_on_event(void* context, SceneManagerEvent event) {
         switch(event.event) {
         case VarItemListIndexScreen:
             scene_manager_next_scene(app->scene_manager, XtremeAppSceneMiscScreen);
+            break;
+        case VarItemListIndexDolphin:
+            scene_manager_next_scene(app->scene_manager, XtremeAppSceneMiscDolphin);
             break;
         case VarItemListIndexChangeDeviceName:
             scene_manager_next_scene(app->scene_manager, XtremeAppSceneMiscRename);
