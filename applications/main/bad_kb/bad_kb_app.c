4ceed8791e280a032b3ecfcfbbe8a9c22f04b518
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-06-11 20:32:23 +0100

    BadKB redraw UI twice as often

--- applications/main/bad_kb/bad_kb_app.c
+++ applications/main/bad_kb/bad_kb_app.c
@@ -155,7 +155,7 @@ BadKbApp* bad_kb_app_alloc(char* arg) {
 
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
     view_dispatcher_set_tick_event_callback(
-        app->view_dispatcher, bad_kb_app_tick_event_callback, 500);
+        app->view_dispatcher, bad_kb_app_tick_event_callback, 250);
     view_dispatcher_set_custom_event_callback(
         app->view_dispatcher, bad_kb_app_custom_event_callback);
     view_dispatcher_set_navigation_event_callback(
