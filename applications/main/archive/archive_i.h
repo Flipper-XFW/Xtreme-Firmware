c70df6753675dac69fc952eb515df08fc6d73014
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-06-19 20:58:51 +0100

    File and folder info go brrrrrrrrr

--- applications/main/archive/archive_i.h
+++ applications/main/archive/archive_i.h
@@ -8,9 +8,11 @@
 #include <gui/scene_manager.h>
 #include <gui/modules/text_input.h>
 #include <gui/modules/widget.h>
+#include <gui/modules/widget_elements/widget_element_i.h>
 #include <gui/view_stack.h>
 #include <dialogs/dialogs.h>
 #include <gui/modules/loading.h>
+#include <toolbox/dir_walk.h>
 #include <loader/loader.h>
 
 #include "views/archive_browser_view.h"
@@ -39,6 +41,9 @@ struct ArchiveApp {
     FuriString* fav_move_str;
     char text_store[MAX_NAME_LEN];
     FuriString* file_extension;
+
+    WidgetElement* element;
+    FuriThread* thread;
 };
 
 void archive_show_loading_popup(ArchiveApp* context, bool show);
