c8ac5cc7b42beafc95d76c01366319a08425c245
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-06-23 00:47:19 +0100

    Revert "Option to preload extmainapps or not"
    This reverts commit 33b8519202ec3cf7932c423954bb62473cdb152b.

--- scripts/fbt/appmanifest.py
+++ scripts/fbt/appmanifest.py
@@ -45,7 +45,6 @@ class FlipperApplication:
 
     appid: str
     apptype: FlipperAppType
-    preload: Optional[bool] = False
     name: Optional[str] = ""
     entry_point: Optional[str] = None
     flags: List[str] = field(default_factory=lambda: ["Default"])
@@ -381,7 +380,7 @@ class ApplicationsCGenerator:
     {{.app = NULL,
      .name = "{app.name}",
      .appid = "/ext/apps/.Main/{app.appid}.fap",
-     .stack_size = {1 if app.preload else 0},
+     .stack_size = 0,
      .icon = {f"&{app.icon}" if app.icon else "NULL"},
      .flags = {'|'.join(f"FlipperApplicationFlag{flag}" for flag in app.flags)}}}"""
         return f"""
