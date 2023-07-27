c20d2344a7fc3ba9c0fccdb37bc24416ff32281e
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-07-27 03:40:56 +0200

    Fix version tag

--- .github/workflow_data/version.sh
+++ .github/workflow_data/version.sh
@@ -2,6 +2,8 @@
 
 export VERSION_TAG="$(python -c '''
 import datetime as dt
+import json
+import os
 with open(os.environ["GITHUB_EVENT_PATH"], "r") as f:
     event = json.load(f)
 version = int(event["pull_request"]["title"].removeprefix("V").removesuffix(" Release").removesuffix(" Hotfix"))
