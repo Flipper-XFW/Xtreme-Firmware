e8f11e55213e5d273e9b22e1b8e2bd940e2ead12
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-08-02 23:33:27 +0200

    Add storage_file_expand()

--- applications/services/storage/storage_message.h
+++ applications/services/storage/storage_message.h
@@ -34,6 +34,11 @@ typedef struct {
 
 typedef struct {
     File* file;
+    uint64_t size;
+} SADataFExpand;
+
+typedef struct {
+    File* file;
     const char* path;
     FuriThreadId thread_id;
 } SADataDOpen;
@@ -97,6 +102,7 @@ typedef union {
     SADataFRead fread;
     SADataFWrite fwrite;
     SADataFSeek fseek;
+    SADataFExpand fexpand;
 
     SADataDOpen dopen;
     SADataDRead dread;
@@ -130,6 +136,7 @@ typedef enum {
     StorageCommandFileWrite,
     StorageCommandFileSeek,
     StorageCommandFileTell,
+    StorageCommandFileExpand,
     StorageCommandFileTruncate,
     StorageCommandFileSize,
     StorageCommandFileSync,
