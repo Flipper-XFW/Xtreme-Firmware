9eae1e7da92a9468d5dc95dd134c07092a845045
eried <1091420+eried@users.noreply.github.com>
2023-09-15 13:14:33 +0200

    Update Mayhem apps
    Removed evil portal (since is included in marauder)

--- applications/external/esp32cam_morseflasher/uart_terminal_app.c
+++ applications/external/esp32cam_morseflasher/uart_terminal_app.c
@@ -56,9 +56,9 @@ UART_TerminalApp* uart_terminal_app_alloc() {
     app->text_box_store = furi_string_alloc();
     furi_string_reserve(app->text_box_store, UART_TERMINAL_TEXT_BOX_STORE_SIZE);
 
-    app->text_input = text_input_alloc();
+    app->text_input = uart_text_input_alloc();
     view_dispatcher_add_view(
-        app->view_dispatcher, UART_TerminalAppViewTextInput, text_input_get_view(app->text_input));
+        app->view_dispatcher, UART_TerminalAppViewTextInput, uart_text_input_get_view(app->text_input));
 
     scene_manager_next_scene(app->scene_manager, UART_TerminalSceneStart);
 
@@ -74,7 +74,7 @@ void uart_terminal_app_free(UART_TerminalApp* app) {
     view_dispatcher_remove_view(app->view_dispatcher, UART_TerminalAppViewTextInput);
     text_box_free(app->text_box);
     furi_string_free(app->text_box_store);
-    text_input_free(app->text_input);
+    uart_text_input_free(app->text_input);
 
     // View dispatcher
     view_dispatcher_free(app->view_dispatcher);
@@ -90,19 +90,20 @@ void uart_terminal_app_free(UART_TerminalApp* app) {
 
 int32_t uart_terminal_app(void* p) {
     UNUSED(p);
-
+    
     // Enable uart listener
     furi_hal_console_disable();
     furi_hal_uart_set_br(UART_CH, BAUDRATE); // TODO: Clean this
     //furi_hal_uart_set_irq_cb(FuriHalUartIdUSART1, uart_echo_on_irq_cb, app);
-
+  
     furi_hal_power_disable_external_3_3v();
     furi_hal_power_disable_otg();
     furi_delay_ms(200);
     furi_hal_power_enable_external_3_3v();
     furi_hal_power_enable_otg();
-    for(int i = 0; i < 2; i++) {
-        furi_delay_ms(500);
+    for(int i=0;i<2;i++)
+    {
+        furi_delay_ms(500); 
         furi_hal_uart_tx(UART_CH, (uint8_t[1]){'.'}, 1);
     }
     furi_delay_ms(1);
@@ -115,6 +116,6 @@ int32_t uart_terminal_app(void* p) {
     uart_terminal_app_free(uart_terminal_app);
 
     furi_hal_power_disable_otg();
-
+    
     return 0;
 }
