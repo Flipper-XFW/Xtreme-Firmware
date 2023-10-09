8926bbcaae952c990f69ca55189b8033fb42c595
HTotoo <ttotoo@gmail.com>
2023-10-09 14:54:15 +0200

    pocsag now displays address

--- lib/subghz/protocols/pocsag.c
+++ lib/subghz/protocols/pocsag.c
@@ -376,6 +376,7 @@ void subhz_protocol_decoder_pocsag_get_string(void* context, FuriString* output)
     furi_assert(context);
     SubGhzProtocolDecoderPocsag* instance = context;
     furi_string_cat_printf(output, "%s\r\n", instance->generic.protocol_name);
+    furi_string_cat_printf(output, "Addr: %lu\r\n", instance->ric);
     furi_string_cat(output, instance->done_msg);
 }
 
