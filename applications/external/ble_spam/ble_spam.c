93cf1433b95c4a9018c671f95897ca3a6610370c
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-10-18 05:25:34 +0100

    BLE Spam icon in app

--- applications/external/ble_spam/ble_spam.c
+++ applications/external/ble_spam/ble_spam.c
@@ -204,7 +204,7 @@ static void draw_callback(Canvas* canvas, void* ctx) {
     const Protocol* protocol = attack ? attack->protocol : NULL;
 
     canvas_set_font(canvas, FontSecondary);
-    canvas_draw_icon(canvas, 4, 3, protocol ? protocol->icon : &I_ble);
+    canvas_draw_icon(canvas, 4 - !protocol, 3, protocol ? protocol->icon : &I_ble_spam);
     canvas_draw_str(canvas, 14, 12, "BLE Spam");
 
     switch(state->index) {
