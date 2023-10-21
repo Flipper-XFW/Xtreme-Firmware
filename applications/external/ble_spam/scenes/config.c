9fb790e5c981ecd5891c53ebeaee19ccb36ce05e
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-10-21 21:47:28 +0100

    Fix some graphical bugs --nobuild

--- applications/external/ble_spam/scenes/config.c
+++ applications/external/ble_spam/scenes/config.c
@@ -48,7 +48,7 @@ void scene_config_on_enter(void* _ctx) {
 
     variable_item_list_set_enter_callback(list, config_callback, ctx);
     if(!ctx->attack->protocol) {
-        variable_item_list_add(list, "None shall escape the S I N K", 0, NULL, NULL);
+        variable_item_list_add(list, "None shall escape the SINK", 0, NULL, NULL);
     } else if(ctx->attack->protocol->extra_config) {
         ctx->fallback_config_enter = config_callback;
         ctx->attack->protocol->extra_config(ctx);
