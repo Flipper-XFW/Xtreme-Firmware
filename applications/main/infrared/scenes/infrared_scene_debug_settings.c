23c40f397a7e4ec8af268b6b96548583dad11c16
Sil333033 <94360907+Sil333033@users.noreply.github.com>
2023-11-20 20:38:30 +0100

    Save Infrared GPIO settings

--- applications/main/infrared/scenes/infrared_scene_debug_settings.c
+++ applications/main/infrared/scenes/infrared_scene_debug_settings.c
@@ -1,5 +1,4 @@
 #include "../infrared_app_i.h"
-#include <furi_hal_infrared.h>
 
 uint8_t value_index_ir;
 
@@ -12,14 +11,17 @@ const char* const infrared_debug_cfg_variables_text[] = {
 static void infrared_scene_debug_settings_changed(VariableItem* item) {
     InfraredApp* infrared = variable_item_get_context(item);
     value_index_ir = variable_item_get_current_value_index(item);
-    UNUSED(infrared);
 
     variable_item_set_current_value_text(item, infrared_debug_cfg_variables_text[value_index_ir]);
 
     furi_hal_infrared_set_debug_out(value_index_ir);
+
+    infrared->last_settings->ext_out = value_index_ir == 1;
+    infrared_last_settings_save(infrared->last_settings);
 }
 
 static void infrared_scene_debug_settings_power_changed(VariableItem* item) {
+    InfraredApp* infrared = variable_item_get_context(item);
     bool value = variable_item_get_current_value_index(item);
     if(value) {
         for(int i = 0; i < 5 && !furi_hal_power_is_otg_enabled(); i++) {
@@ -32,6 +34,9 @@ static void infrared_scene_debug_settings_power_changed(VariableItem* item) {
         }
     }
     variable_item_set_current_value_text(item, value ? "ON" : "OFF");
+
+    infrared->last_settings->ext_5v = value;
+    infrared_last_settings_save(infrared->last_settings);
 }
 
 static void infrared_debug_settings_start_var_list_enter_callback(void* context, uint32_t index) {
@@ -64,7 +69,8 @@ void infrared_scene_debug_settings_on_enter(void* context) {
         2,
         infrared_scene_debug_settings_power_changed,
         infrared);
-    bool enabled = furi_hal_power_is_otg_enabled();
+    bool enabled = furi_hal_power_is_otg_enabled() ||
+                   furi_hal_power_is_charging(); // 5v is enabled via hardware if charging
     variable_item_set_current_value_index(item, enabled);
     variable_item_set_current_value_text(item, enabled ? "ON" : "OFF");
 
