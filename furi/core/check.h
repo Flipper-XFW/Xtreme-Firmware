f3ac15acdc24b93677419af412275cec7060b33b
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-01-18 07:41:48 +0000

    Fix updater bin being phat (for now)

--- furi/core/check.h
+++ furi/core/check.h
@@ -23,7 +23,9 @@ extern "C" {
 #define FURI_NORETURN noreturn
 #endif
 
+#ifndef FURI_RAM_EXEC
 #define __FURI_TRACE
+#endif
 
 // Flags instead of pointers will save ~4 bytes on furi_assert and furi_check calls.
 #ifndef __FURI_TRACE
