18579abbcf5f5792069bb4c1d095b3600777593a
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-01-30 19:25:12 +0000

    Revert "Default expansion UART listen to off for now"
    This reverts commit bbcff4851761765a7013c305c32f872a7944f5f6.

--- applications/services/expansion/expansion_settings.c
+++ applications/services/expansion/expansion_settings.c
@@ -2,7 +2,6 @@
 
 #include <storage/storage.h>
 #include <toolbox/saved_struct.h>
-#include <furi_hal_serial.h>
 
 #include "expansion_settings_filename.h"
 
@@ -11,15 +10,12 @@
 
 bool expansion_settings_load(ExpansionSettings* settings) {
     furi_assert(settings);
-    if(!saved_struct_load(
-           EXPANSION_SETTINGS_PATH,
-           settings,
-           sizeof(ExpansionSettings),
-           EXPANSION_SETTINGS_MAGIC,
-           EXPANSION_SETTINGS_VERSION)) {
-        settings->uart_index = FuriHalSerialIdMax;
-    }
-    return true;
+    return saved_struct_load(
+        EXPANSION_SETTINGS_PATH,
+        settings,
+        sizeof(ExpansionSettings),
+        EXPANSION_SETTINGS_MAGIC,
+        EXPANSION_SETTINGS_VERSION);
 }
 
 bool expansion_settings_save(ExpansionSettings* settings) {
