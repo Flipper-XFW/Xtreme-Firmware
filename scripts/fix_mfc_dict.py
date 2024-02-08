134e74dda3fb5e35c7b70969cbe949ebd1f1dbb0
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-02-08 23:46:10 +0000

    Upd MFC keys: added 41 from RRG pm3

--- scripts/fix_mfc_dict.py
+++ scripts/fix_mfc_dict.py
@@ -2,8 +2,8 @@ import pathlib
 import string
 
 file = (
-    pathlib.Path(__file__)
-    / "../../applications/main/nfc/resources/nfc/assets/mf_classic_dict.nfc"
+    pathlib.Path(__file__).parent
+    / "../applications/main/nfc/resources/nfc/assets/mf_classic_dict.nfc"
 )
 try:
     lines = file.read_text(encoding="ascii").splitlines()
@@ -14,13 +14,16 @@ except UnicodeDecodeError:
     exit()
 
 for i, line in enumerate(lines):
+    lines[i] = line = line.strip()
     if line.startswith("#"):
         continue
     lines[i] = line = line.upper()
+    if not line:
+        continue
     if len(line) != 12 or any(char not in string.hexdigits for char in line):
-        print(f"line {i} is not correct: {line}")
+        print(f"line {i + 1} is not correct: {line}")
     for j in reversed(range(i + 1, len(lines))):
-        if lines[j].upper() == line:
+        if lines[j].upper().strip() == line:
             del lines[j]
 
-file.write_text("\n".join(lines))
+file.write_text("\n".join(line for line in lines if line.removeprefix("#")))
