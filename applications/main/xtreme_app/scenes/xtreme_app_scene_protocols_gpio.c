14073941e63712dc82073490ef61263ad7b94b28
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-02-13 01:31:36 +0000

    Clearer GPIO Pins section in Xtreme app

--- applications/main/xtreme_app/scenes/xtreme_app_scene_protocols_gpio.c
+++ applications/main/xtreme_app/scenes/xtreme_app_scene_protocols_gpio.c
@@ -8,6 +8,11 @@ enum VarItemListIndex {
     VarItemListIndexUartGeneralChannel,
 };
 
+#define SPI_DEFAULT "Default 4"
+#define SPI_EXTRA "Extra 7"
+#define UART_DEFAULT "Default 13,14"
+#define UART_EXTRA "Extra 15,16"
+
 void xtreme_app_scene_protocols_gpio_var_item_list_callback(void* context, uint32_t index) {
     XtremeApp* app = context;
     view_dispatcher_send_custom_event(app->view_dispatcher, index);
@@ -18,7 +23,7 @@ static void xtreme_app_scene_protocols_gpio_cc1101_handle_changed(VariableItem* 
     xtreme_settings.spi_cc1101_handle =
         variable_item_get_current_value_index(item) == 0 ? SpiDefault : SpiExtra;
     variable_item_set_current_value_text(
-        item, xtreme_settings.spi_cc1101_handle == SpiDefault ? "Default" : "Extra");
+        item, xtreme_settings.spi_cc1101_handle == SpiDefault ? SPI_DEFAULT : SPI_EXTRA);
     app->save_settings = true;
 }
 
@@ -27,7 +32,7 @@ static void xtreme_app_scene_protocols_gpio_nrf24_handle_changed(VariableItem* i
     xtreme_settings.spi_nrf24_handle =
         variable_item_get_current_value_index(item) == 0 ? SpiDefault : SpiExtra;
     variable_item_set_current_value_text(
-        item, xtreme_settings.spi_nrf24_handle == SpiDefault ? "Default" : "Extra");
+        item, xtreme_settings.spi_nrf24_handle == SpiDefault ? SPI_DEFAULT : SPI_EXTRA);
     app->save_settings = true;
 }
 
@@ -37,7 +42,8 @@ static void xtreme_app_scene_protocols_gpio_esp32_channel_changed(VariableItem* 
                                            FuriHalSerialIdUsart :
                                            FuriHalSerialIdLpuart;
     variable_item_set_current_value_text(
-        item, xtreme_settings.uart_esp_channel == FuriHalSerialIdUsart ? "13,14" : "15,16");
+        item,
+        xtreme_settings.uart_esp_channel == FuriHalSerialIdUsart ? UART_DEFAULT : UART_EXTRA);
     app->save_settings = true;
 }
 
@@ -47,7 +53,8 @@ static void xtreme_app_scene_protocols_gpio_nmea_channel_changed(VariableItem* i
                                             FuriHalSerialIdUsart :
                                             FuriHalSerialIdLpuart;
     variable_item_set_current_value_text(
-        item, xtreme_settings.uart_nmea_channel == FuriHalSerialIdUsart ? "13,14" : "15,16");
+        item,
+        xtreme_settings.uart_nmea_channel == FuriHalSerialIdUsart ? UART_DEFAULT : UART_EXTRA);
     app->save_settings = true;
 }
 
@@ -57,7 +64,8 @@ static void xtreme_app_scene_protocols_gpio_general_channel_changed(VariableItem
                                                FuriHalSerialIdUsart :
                                                FuriHalSerialIdLpuart;
     variable_item_set_current_value_text(
-        item, xtreme_settings.uart_general_channel == FuriHalSerialIdUsart ? "13,14" : "15,16");
+        item,
+        xtreme_settings.uart_general_channel == FuriHalSerialIdUsart ? UART_DEFAULT : UART_EXTRA);
     app->save_settings = true;
 }
 
@@ -67,54 +75,49 @@ void xtreme_app_scene_protocols_gpio_on_enter(void* context) {
     VariableItem* item;
 
     item = variable_item_list_add(
-        var_item_list,
-        "SPI CC1101 Handle",
-        2,
-        xtreme_app_scene_protocols_gpio_cc1101_handle_changed,
-        app);
+        var_item_list, "CC1101 SPI", 2, xtreme_app_scene_protocols_gpio_cc1101_handle_changed, app);
     variable_item_set_current_value_index(item, xtreme_settings.spi_cc1101_handle);
     variable_item_set_current_value_text(
-        item, xtreme_settings.spi_cc1101_handle == SpiDefault ? "Default" : "Extra");
+        item, xtreme_settings.spi_cc1101_handle == SpiDefault ? SPI_DEFAULT : SPI_EXTRA);
 
     item = variable_item_list_add(
-        var_item_list,
-        "SPI NRF24 Handle",
-        2,
-        xtreme_app_scene_protocols_gpio_nrf24_handle_changed,
-        app);
+        var_item_list, "NRF24 SPI", 2, xtreme_app_scene_protocols_gpio_nrf24_handle_changed, app);
     variable_item_set_current_value_index(item, xtreme_settings.spi_nrf24_handle);
     variable_item_set_current_value_text(
-        item, xtreme_settings.spi_nrf24_handle == SpiDefault ? "Default" : "Extra");
+        item, xtreme_settings.spi_nrf24_handle == SpiDefault ? SPI_DEFAULT : SPI_EXTRA);
 
     item = variable_item_list_add(
         var_item_list,
-        "UART ESP32/ESP8266 Channel",
+        "ESP32/8266 UART",
         2,
         xtreme_app_scene_protocols_gpio_esp32_channel_changed,
         app);
     variable_item_set_current_value_index(item, xtreme_settings.uart_esp_channel);
     variable_item_set_current_value_text(
-        item, xtreme_settings.uart_esp_channel == FuriHalSerialIdUsart ? "13,14" : "15,16");
+        item,
+        xtreme_settings.uart_esp_channel == FuriHalSerialIdUsart ? UART_DEFAULT : UART_EXTRA);
 
     item = variable_item_list_add(
         var_item_list,
-        "UART NMEA Channel",
+        "NMEA GPS UART",
         2,
         xtreme_app_scene_protocols_gpio_nmea_channel_changed,
         app);
     variable_item_set_current_value_index(item, xtreme_settings.uart_nmea_channel);
     variable_item_set_current_value_text(
-        item, xtreme_settings.uart_nmea_channel == FuriHalSerialIdUsart ? "13,14" : "15,16");
+        item,
+        xtreme_settings.uart_nmea_channel == FuriHalSerialIdUsart ? UART_DEFAULT : UART_EXTRA);
 
     item = variable_item_list_add(
         var_item_list,
-        "UART General Channel",
+        "General UART",
         2,
         xtreme_app_scene_protocols_gpio_general_channel_changed,
         app);
     variable_item_set_current_value_index(item, xtreme_settings.uart_general_channel);
     variable_item_set_current_value_text(
-        item, xtreme_settings.uart_general_channel == FuriHalSerialIdUsart ? "13,14" : "15,16");
+        item,
+        xtreme_settings.uart_general_channel == FuriHalSerialIdUsart ? UART_DEFAULT : UART_EXTRA);
 
     variable_item_list_set_enter_callback(
         var_item_list, xtreme_app_scene_protocols_gpio_var_item_list_callback, app);
